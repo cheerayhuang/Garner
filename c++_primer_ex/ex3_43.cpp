16f7f732520ace15ed69ee7e352e034f4a2a36f5
cheearyhuang <cheeray.huang@gmail.com>
2014-08-12 12:12:32 +0800

    [add] exercises in c++ primer 	new file:   ex3_43.cpp 	new file:   ex3_44.cpp 	new file:   ex3_45.cpp

---
+++ c++_primer_ex/ex3_43.cpp
@@ -0,0 +1,39 @@
+/**************************************************************************
+ *
+ * Copyright (c) 2014 Nibirutech, Inc. All Rights Reserved
+ * www.Nibirutech.com
+ * 
+ * @file: ex3_43.cpp
+ * @author: Huang Qiyu
+ * @email: huangqiyu@chukong-inc.com
+ * @date: 08-12-2014 11:02:22
+ * @version $Revision$
+ *
+ **************************************************************************/
+
+#include <iostream> 
+
+using namespace std; 
+
+int main() {
+
+    int ia[3][4]; 
+
+    cout << "version 1" << endl;
+    for (int (&i)[4] : ia)
+        for (int j : i) 
+        cout << j << endl ;
+    
+    cout << "version 2" << endl; 
+    for (int i = 0; i < 3; ++i) 
+        for(int j = 0; j < 4; ++j) 
+            cout << ia[i][j] << endl;
+    
+    cout << "version 3" << endl;
+    for (int (*i)[4] = ia; i < ia + 3; ++i) 
+        for (int *j = *i; j < *i + 4; ++j) 
+            cout << *j << endl; 
+
+    return 0;
+}
+
