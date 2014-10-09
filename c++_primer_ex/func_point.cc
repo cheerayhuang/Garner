e47ae27d9603439a7eda7a75745f325db271028d
cheearyhuang <cheeray.huang@gmail.com>
2014-10-09 17:03:49 +0800

    new file:   func_point.cc

---
+++ c++_primer_ex/func_point.cc
@@ -0,0 +1,25 @@
+/**************************************************************************
+ *
+ * Copyright (c) 2014 Nibirutech, Inc. All Rights Reserved
+ * www.Nibirutech.com
+ * 
+ * @file: func_point.cc
+ * @author: Huang Qiyu
+ * @email: huangqiyu@chukong-inc.com
+ * @date: 10-09-2014 17:00:51
+ * @version $Revision$
+ *
+ **************************************************************************/
+
+#include <iostream> 
+
+using namespace std; 
+
+typedef int func(int *, int);
+
+func* f1(int);
+
+int main() {
+    return 0;
+}
+
