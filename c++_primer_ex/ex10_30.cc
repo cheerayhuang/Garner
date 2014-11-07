a4973036639d5b82101c5ca2922cd1258d0caf75
cheearyhuang <cheeray.huang@gmail.com>
2014-11-07 11:50:43 +0800

    new file:   ex10_30.cc 	new file:   ex10_33.cc 	new file:   ex10_33.txt 	new file:   ex10_33_even.txt 	new file:   ex10_33_odd.txt

---
+++ c++_primer_ex/ex10_30.cc
@@ -0,0 +1,31 @@
+/**************************************************************************
+ *
+ * Copyright (c) 2014 Nibirutech, Inc. All Rights Reserved
+ * www.Nibirutech.com
+ * 
+ * @file: ex10_30.cc
+ * @author: Huang Qiyu
+ * @email: huangqiyu@chukong-inc.com
+ * @date: 11-07-2014 10:54:33
+ * @version $Revision$
+ *
+ **************************************************************************/
+
+#include <iostream> 
+#include <iterator>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    istream_iterator<int> is_iter = cin, eof;
+    ostream_iterator<int> os_iter(cout, " ");
+
+    vector<int>v (is_iter, eof);
+    sort(v.begin(), v.end());
+    copy(v.begin(), v.end(), os_iter);
+
+    return 0;
+}
+
---
+++ c++_primer_ex/ex10_33.cc
@@ -0,0 +1,53 @@
+/**************************************************************************
+ *
+ * Copyright (c) 2014 Nibirutech, Inc. All Rights Reserved
+ * www.Nibirutech.com
+ * 
+ * @file: ex10_33.cc
+ * @author: Huang Qiyu
+ * @email: huangqiyu@chukong-inc.com
+ * @date: 11-07-2014 11:20:14
+ * @version $Revision$
+ *
+ **************************************************************************/
+
+#include <fstream>
+#include <iterator>
+#include <algorithm>
+#include <iostream>
+#include <functional>
+
+using namespace std;
+using namespace placeholders;
+
+auto func(const int& i, int odd_or_even = 0) ->bool {
+    if (i%2 != odd_or_even) {
+        return true;
+    }
+    return false;
+}
+
+int main() {
+    ifstream fin("ex10_33.txt");
+    ofstream fout_odd("ex10_33_odd.txt");
+    ofstream fout_even("ex10_33_even.txt");
+
+    istream_iterator<int> is_iter(fin), eof;
+    ostream_iterator<int> os_iter_odd(fout_odd, " ");
+    ostream_iterator<int> os_iter_even(fout_even, "\n");
+
+    vector<int> v(is_iter, eof);
+
+    /*copy_if(v.begin(), v.end(), os_iter_odd, [](int &i) { if (i % 2) { return true; } return false; });
+    copy_if(v.begin(), v.end(), os_iter_even, [](int &i) { if (!(i % 2)) return true; return false; });*/
+
+    copy_if(v.begin(), v.end(), os_iter_odd, bind(func, _1, 0));
+    copy_if(v.begin(), v.end(), os_iter_even, bind(func, _1, 1));
+    
+    fin.close();
+    fout_odd.close();
+    fout_even.close();
+    
+    return 0;
+}
+
