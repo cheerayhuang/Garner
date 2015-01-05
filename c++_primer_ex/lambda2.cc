2d98d5467ea602e7a4bacd4cc97358904b7366d6
cheearyhuang <cheeray.huang@gmail.com>
2015-01-05 16:24:10 +0800

    renamed:    lamabda2.cc -> lambda2.cc

--- c++_primer_ex/lambda2.cc
+++ c++_primer_ex/lambda2.cc
@@ -18,10 +18,18 @@ using namespace std;
 int main() {
     size_t v1 = 42;
 
-    auto f = [&v1]()->int { if (1+1==3) {return 0;} return ++v1; };
+    auto f = [v1]() mutable ->int  { if (1+1==3) {return 0;} return ++v1; };
     v1 = 0;
 
     cout << f() << endl;
+
+    cout << v1 << endl;
+
+    using vv = decltype(v1);
+
+    vv vv1 = -1;
+
+    cout << vv1 << endl;
 
     return 0;
 }
