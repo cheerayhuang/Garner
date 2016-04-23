3843c6a94154a9d99f738671132095845fa3cf8d
cheerayhuang <cheeray.huang@gmail.com>
2016-04-23 22:02:12 +0800

    new file:   ex15_11.cc 	new file:   ex15_16.cc 	new file:   ex15_7.cc

---
+++ c++_primer_ex/ex15_7.cc
@@ -0,0 +1,82 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Quote {
+public:
+    Quote() = default;
+    Quote(const string &book, double sales_price):
+        book_no_(book), price_(sales_price) {}
+
+    const string& book_no() const {
+        return book_no_;
+    }
+
+    virtual double NetPrice(std::size_t n) const {
+        return n * price_;
+    }
+
+private:
+    string book_no_;
+
+protected:
+    double price_ = 0.0;
+};
+
+class BulkQuote : public Quote {
+public:
+    BulkQuote() = default;
+    BulkQuote(const string &book, double sales_price, std::size_t qty, double discount):
+        Quote(book, sales_price), min_qty(qty), discount(discount) {
+    }
+
+    double NetPrice(std::size_t n) const {
+        if (n >= min_qty) {
+            return n * discount * price_;
+        }
+        else {
+            return n * price_;
+        }
+    }
+
+private:
+    size_t min_qty = 0;
+    double discount = 0;
+};
+
+class LimitQuote : public Quote {
+public:
+    LimitQuote() = default;
+    LimitQuote(const string &book, double sales_price, std::size_t qty, double discount):
+        Quote(book, sales_price), min_qty(qty), discount(discount) {
+        }
+
+    double NetPrice(std::size_t n) const {
+        if (n <= min_qty) {
+            return n * discount * price_;
+        }
+        else {
+            return min_qty * discount * price_ + (n-min_qty) * price_;
+        }
+    }
+
+private:
+    size_t min_qty = 0;
+    double discount = 0;
+};
+
+int main() {
+
+    Quote q("book1", 10.0);
+
+    BulkQuote b("book2", 11.1, 10, 0.3);
+
+    Quote *p = &b;
+    cout << p->NetPrice(10) << endl;
+
+    const Quote &r = LimitQuote("book3", 12.5, 10, 0.4);
+    cout << r.NetPrice(11) << endl;
+
+    return 0;
+}
