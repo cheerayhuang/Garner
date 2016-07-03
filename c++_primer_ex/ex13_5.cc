ca4bf86257a8975893c1b9f226b618b9a67f690f
cheeray huang <cheeray.huang@gmail.com>
2016-07-03 22:36:29 +0800

    new file:   ex13_5.cc

---
+++ c++_primer_ex/ex13_5.cc
@@ -0,0 +1,53 @@
+/**************************************************************************
+ *
+ * Copyright (c) 2015 Cheeray Huang. All Rights Reserved
+ *
+ * @file: ex13_5.cc
+ * @author: Huang Qiyu
+ * @email: cheeray.huang@gmail.com
+ * @date: 07-03-2016 22:11:24
+ * @version $Revision$
+ *
+ **************************************************************************/
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class HasPtr {
+
+public:
+    HasPtr(const string &s = string()):
+        ps(new string(s)), i(0) {}
+
+    HasPtr(const HasPtr& h);
+
+//private:
+public:
+    string *ps;
+    int i;
+};
+
+HasPtr::HasPtr(const HasPtr& h) {
+    auto s = *(h.ps);
+    delete h.ps;
+
+    i = h.i;
+    ps = new string(s);
+
+    cout << "copy construct" << endl;
+}
+
+
+int main() {
+    HasPtr h1("hello");
+    HasPtr h2 = h1;
+    HasPtr h3(h1);
+
+    cout << *(h2.ps) << endl;
+    cout << *(h3.ps) << endl;
+
+    return 0;
+}
+
